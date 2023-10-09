ffc3ac2c34b22e6940d95c6e06fcaeab3a7545a5
Fikri-Ramadan <fikrinr.tech@gmail.com>
2023-10-10 00:25:59 +0700

    create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,30 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <section className="f-wrapper">
+      <div className="paddings innerWidth f-container">
+        <div className="f-left">
+          <div className="logo">
+            <img src="./logo2.png" alt="" width={250} />
+          </div>
+          <span className="secondaryText">
+            Our vision is to make all people <br />
+            the best place to live for them
+          </span>
+        </div>
+        <div className="f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">Jakarta, Indonesia</span>
+          <div className="f-menu">
+            <span>property</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Footer;
