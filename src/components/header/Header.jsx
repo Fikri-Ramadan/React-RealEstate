f1f649bec4740a552b026a59855475cad86f05c2
Fikri Ramadan <fikrinr.tech@gmail.com>
2023-10-26 14:13:28 +0700

    (feature) - Login
    create login feature using auth0 & UI using mantine

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,8 @@ import './Header.css';
 import { BiMenuAltRight } from 'react-icons/bi';
 import OutsideClickHandler from 'react-outside-click-handler';
 import { Link, NavLink } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import ProfileMenu from '../profileMenu/ProfileMenu';
 
 // const getMenuStyles = (menuOpen) => {
 //   if (document.documentElement.clientWidth <= 800) {
@@ -12,6 +14,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
 
   return (
     <section className="h-wrapper">
@@ -27,7 +30,13 @@ const Header = () => {
           <div className={`h-menu ${isOpen && 'open-menu'}`}>
             <NavLink to="/properties">Properties</NavLink>
             <a href="mailto:fikriramadan.tech@gmail.com">Contact</a>
-            <button className="button">Login</button>
+            {isAuthenticated ? (
+              <ProfileMenu user={user} logout={logout} />
+            ) : (
+              <button className="button" onClick={() => loginWithRedirect()}>
+                Login
+              </button>
+            )}
           </div>
 
           <div
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: import.meta.env.VITE_BASEURL,
+      }}
+      audience={import.meta.env.VITE_API_BASEURL}
+      scope="openid profile email"
+    >
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
